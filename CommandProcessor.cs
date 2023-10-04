6abb403e41909b3bc372212c59799d85f25bb972
JKaliraj <rajutechpro2020@gmail.com>
2023-10-04 13:09:56 +0530

    Add project files.

---
+++ CommandProcessor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Cosmos;
+namespace ZeroOS_Horizon
+{
+    public class CommandProcessor
+    {
+        private List<string> history;
+
+        public CommandProcessor(List<string> history)
+        {
+            this.history = history;
+        }
+
+        public void ProcessCommand(string input)
+        {
+            history.Add(input);
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+
+            string[] parts = input.Split(' ');
+            string command = parts[0].ToLower();
+
+            switch (command)
+            {
+                case "echo":
+                    if (parts.Length > 1)
+                    {
+                        string text = string.Join(" ", parts, 1, parts.Length - 1);
+                        Console.WriteLine("Echo: " + text);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Usage: echo [text]");
+                    }
+                    break;
+
+                case "help":
+                    Console.WriteLine("Available commands: ");
+                    Console.WriteLine("echo [text] - Echo the given text.");
+                    Console.WriteLine("history - Show command history.");
+                    break;
+
+                case "history":
+                    Console.WriteLine("Command history:");
+                    for (int i = 0; i < history.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}: {history[i]}");
+                    }
+                    break;
+                case "shutdown":
+                    Console.WriteLine("Shutting down the system...");
+                    Cosmos.System.Power.Shutdown();
+                    break;
+
+                case "restart":
+                    Console.WriteLine("Restarting the system...");
+                    Cosmos.System.Power.Shutdown();
+                    break;
+                default:
+                    Console.WriteLine("Unknown command. Type 'help' for a list of available commands.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace ZeroOS_Horizon
+{
+    public class Kernel : Sys.Kernel
+    {
+        private List<string> history = new List<string>();
+        private CommandProcessor commandProcessor;
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();  // Clear the console
+            Console.ForegroundColor = ConsoleColor.Cyan; // Set text color to Cyan
+            Console.BackgroundColor = ConsoleColor.DarkBlue; // Set background color to Dark Blue
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine("*     Welcome to ZeroOS-Horizon      *");
+            Console.WriteLine("**************************************");
+
+            Console.ResetColor(); // Reset colors to default
+
+            commandProcessor = new CommandProcessor(history);
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            commandProcessor.ProcessCommand(input);
+        }
+    }
+}
